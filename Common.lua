23822c0e3f57308688dc1c3c28a23c8533b9a9aa
zhuWin <86637775+zhuWin@users.noreply.github.com>
2022-03-30 22:53:10 +0800

    Add files via upload

---
+++ Common.lua
@@ -0,0 +1,37 @@
+--region ShellCommon.lua
+--Date 2015-12-15
+
+----------------------°üº¬ÎÄ¼þ----------------------
+require "misc/CBufferStream"
+require "misc/CEventDispatcher"
+require "misc/ConfigLoadHelp"
+require "misc/CTick"
+require "misc/GammaMath"
+require "misc/Help"
+require "misc/Hash"
+require "misc/json"
+require "misc/CommonConstDefine"
+
+require "protocal/PrtShellClientWorldID"
+require "protocal/PrtShellClientRankID"
+require "protocal/PrtShellClientFieldID"
+require "protocal/PrtShellWorldFieldID"
+require "protocal/PrtBase"
+require "protocal/PrtShellRegister"
+require "protocal/PrtShellClient2World"
+require "protocal/PrtShellClient2Field"
+require "protocal/PrtShellClient2Rank"
+require "protocal/PrtShellWorld2Client"
+require "protocal/PrtShellField2Client"
+require "protocal/PrtShellRank2Client"
+require "protocal/PrtShellWorld2Field"
+require "protocal/PrtShellField2World"
+
+require "debug/CCmdMgrBase"
+
+require "scene/CGameScene"
+require "character/CPlayer"
+require "character/CCharacter"
+
+require "wanfa/CWanfaBase"
+--endregion
---
+++ CommonInc.lua
@@ -0,0 +1,11 @@
+--region CommonInc.lua
+--Date 2015-12-07
+
+CCmdMgrBase = class()
+
+CEventDispatcher = class();
+CLoadingListener = class( IResListener );
+
+CWanfaBase = class();
+
+--endregion
